29b8a3700ed353bf39fe7fec51bb14ae3afc35f4
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-10-13 07:24:48 +0200

    added modules

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+
+
 //Declaring the api url that will provide data for the client app
 const apiUrl = 'https://movie-app-svs.herokuapp.com/';
 @Injectable({
